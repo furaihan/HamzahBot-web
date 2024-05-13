11063e5f74e5dae568a54a5d9c94f9be8d7693ff
furaihan <28698437+furaihan@users.noreply.github.com>
2024-05-13 20:08:06 +0700

    nim nama

--- src/App.svelte
+++ src/App.svelte
@@ -5,43 +5,31 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <!-- Create table with NIM, and Nama -->
+    <table>
+      <tr>
+        <th>NIM</th>
+        <th>Nama</th>
+      </tr>
+      <tr>
+        <td>21.11.4352</td>
+        <td>Reksa Panduasa</td>
+      </tr>
+      <tr>
+        <td>21.11.4374</td>
+        <td>Muhammad Zhafar Al Fathi</td>
+      </tr>
+      <tr>
+        <td>21.11.4396</td>
+        <td>Dicky Akbar Baihaqki</td>
+      </tr>
+      <tr>
+        <td>21.11.4398</td>
+        <td>Irfan Andryanto</td>
+      </tr>
+    </table>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
