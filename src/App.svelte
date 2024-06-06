3e8623c106a26c14b34782a1c8d3586eeab3d932
Zhafar Al Fathi <28698437+furaihan@users.noreply.github.com>
2024-06-06 20:41:14 +0700

    feat(asset): move asset to public directory

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,6 @@
 <script>
   import HeroSection from './HeroSection.svelte';
   import NavbarItemsDesktop from './NavbarItemsDesktop.svelte';
-  import hero1 from './assets/hero1.png';
-  import hero2 from './assets/hero2.svg';
-  import hero3 from './assets/hero3.png';
-  import hero4 from './assets/hero4.png';
-  import discordBlue from './assets/discord-mark-blue.svg';
-  import discordWhite from './assets/discord-mark-white.svg';
   import { onMount } from 'svelte';
 
   let menuOpen = false;
@@ -53,8 +47,8 @@
         <div class="flex flex-row items-center space-y-0 space-x-4">
           <button class="group w-full md:w-auto backdrop-blur-sm bg-discord text-white px-4 py-2 rounded-3xl flex flex-row items-center space-x-2 border border-white hover:bg-white hover:text-discord transition delay-100 ease-in-out hover:-translate-y-1 hover:scale-110">
             <div class="aspect-auto items-center">
-              <img src="{discordWhite}" alt="discord" class="w-6 group-hover:hidden">
-              <img src="{discordBlue}" alt="discord" class="w-6 hidden group-hover:block">
+              <img src="discord-mark-white.svg" alt="discord" class="w-6 group-hover:hidden">
+              <img src="discord-mark-blue.svg" alt="discord" class="w-6 hidden group-hover:block">
             </div>
             <p>Login with Discord</p>
           </button>
@@ -104,26 +98,26 @@
       <HeroSection 
         title="Kelola server Anda dengan lebih baik" 
         content="Satu fitur manajemen dengan pesan yang dipersonalisasi, pengaturan server pintar, dan bot yang dapat melakukan tugas berkala. Kelas voice channel dengan temporary voice, dan undang teman-teman Anda dari acara di online. Dapatkan fitur-fitur canggih untuk pengalaman server Discord yang lebih baik."
-        imageUrl="{hero1}"
+        imageUrl="hero1.png"
         isImageLeft={false}
       />
       <HeroSection 
         title="Asah otak Anda dengan Wordle Bahasa Indonesia" 
         content="Mainkan Wordle hari-hari dalam versi bahasa Indonesia. Kerahkan game ini untuk mengasah kosa kata bahasa Indonesia, melatih otak, dan bersaing dengan teman-teman."
-        imageUrl="{hero2}"
+        imageUrl="hero2.svg"
         isImageLeft={true} 
       />
       <HeroSection 
         title="Promosikan saluran streaming Anda" 
         content="Layanan yang memberikan pemberitahuan kepada pengguna tentang sorotan baru dan penting dari saluran streaming yang mereka sukai. Melalui notifikasi, pengguna dapat tetap terinformasi secara real-time tentang video-video atau siaran yang akan dimulai."
-        imageUrl="{hero3}"
+        imageUrl="hero3.png"
         isImageLeft={false} 
         aspectRatio="4/3"
       />
       <HeroSection 
         title="Ubah imajinasi menjadi nyata dengan AI" 
         content="Uji coba kemampuan AI sistem mengubah gambar untuk gambar seni dan kreatif. Rasakan bot kami mentransformasi sentuhan magis pada foto-foto teman!"
-        imageUrl="{hero4}"
+        imageUrl="hero4.png"
         isImageLeft={true} 
       />
   </section>
@@ -131,7 +125,7 @@
   <!-- Call to Action Section -->
   <section class="flex items-center justify-center py-10 bg-discord">
     <button class="transition flex items-center bg-white font-bold px-4 md:px-6 py-3 rounded-2xl text-sm md:text-base hover:-translate-y-2 ease-in-out">
-      <img src="{discordBlue}" alt="discord" class="w-6">
+      <img src="discord-mark-blue.svg" alt="discord" class="w-6">
       <span class="ml-2 text-discord">Add to Discord</span>
     </button>
   </section>
